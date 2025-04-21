2f01cd2a9f6828346652ab50932f1a1b2837fd87
Ivan Dalyuk <107864230+I-K-DLK@users.noreply.github.com>
2025-04-21 12:07:28 +0300

    Add files via upload

---
+++ Подключение к внешним источникам и создание таблиц.sql
@@ -0,0 +1,92 @@
+
+-- Создадим новые таблицы Plan  и Sales со структурой аналогичных внешних таблиц из БД Postgres
+
+
+-- Партиционирование таблиц фактов выбираем помесячное исходя из периодичности построения отчетности в конце текущего месяца
+-- Распределение таблиц производим выбирая поле распределение, обеспечивающее наименьший перекос данных и по возможности учитывая
+-- возможные поля для Joins
+
+-- DROP TABLE std11_61.plan if exists;
+
+CREATE TABLE std11_61.plan (
+	"date" date,
+	region varchar(20),
+	matdirec varchar(20),
+	quantity int4,
+	distr_chan varchar(100)
+	
+)
+DISTRIBUTED RANDOMLY
+PARTITION BY RANGE("date")
+(
+PARTITION ym START ('2021-01-01'::date) END ('2021-08-01'::date) EVERY ('1 mon'::interval) 
+WITH (appendoptimized=true, orientation=column, compresslevel=1, compresstype=zstd)
+);
+ 
+
+
+-- DROP TABLE gp.sales if exists;
+
+CREATE TABLE std11_61.sales (
+	"date" date,
+	region varchar(20),
+	material varchar(20),
+	distr_chan varchar(100),
+	quantity int4,
+	check_nm varchar(100),
+	check_pos varchar(100)
+	
+)
+DISTRIBUTED RANDOMLY
+PARTITION BY RANGE("date")
+(
+PARTITION ym START ('2021-01-01'::date) END ('2021-08-01'::date) EVERY ('1 mon'::interval) 
+WITH (appendoptimized=true, orientation=column, compresslevel=1, compresstype=zstd)
+);
+-- Создадим новые таблицы 
+
+-- 1) price
+
+-- DROP TABLE if exists std11_61.price ;
+
+CREATE TABLE std11_61.price (
+	material varchar(20),
+	region varchar(20),
+	distr_chan varchar(100),
+	price int
+)
+DISTRIBUTED REPLICATED;
+
+-- 2) product
+
+-- DROP TABLE if exists std11_61.product ;
+
+CREATE TABLE std11_61.product (
+	material varchar(20),
+	asgrp int,
+	brand int,
+	matcateg varchar(4),
+	matdirec varchar(20),
+	txt text
+)
+DISTRIBUTED REPLICATED;
+
+-- 3) chanel
+
+-- DROP TABLE if exists std11_61.chanel ;
+
+CREATE TABLE std11_61.chanel (
+	distr_chan varchar(100),
+	txtsh text
+)
+DISTRIBUTED REPLICATED;
+
+-- 4) region
+
+-- DROP TABLE if exists std11_61.region ;
+
+CREATE TABLE std11_61.region (
+	region varchar(20),
+	txt text
+)
+DISTRIBUTED REPLICATED;
